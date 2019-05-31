245f386761fbd51771a36f662e1f8f4adc348213
Eliagandolfi <47365996+Eliagandolfi@users.noreply.github.com>
2019-05-31 17:18:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const { Client } = require('pg'); 
+express.get('/db', (req, res) => {
+var dbOpts = {
+      connectionString: process.env.DATABASE_URL, 
+      ssl : true
+    }
+const client = new Client(dbOpts);    
+client.connect();    
+client.query('SELECT full_name__c FROM salesforce.Client__c;', (err, dbRes) => {
+      if (err) throw err;     
+ 
+      res.render('pages/db',{
+        results : dbRes.rows
+      });
+ 
+      client.end();
+    });
+ 
+client.end();    
+})
